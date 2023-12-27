aa535134b0c87e41b34fc64095855e705201072c
dg266 <daniele.galloppo@protonmail.com>
2023-12-27 01:07:37 +0100

    Initial commit

---
+++ src/test/java/it/unisa/yawa/YawaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.yawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
