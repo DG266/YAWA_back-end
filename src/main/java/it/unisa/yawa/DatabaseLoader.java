627e3ba057de824cda67b12767ef7b2a3d772da2
dg266 <daniele.galloppo@protonmail.com>
2023-12-27 19:24:28 +0100

    Add DatabaseLoader in order to pre-load some data

---
+++ src/main/java/it/unisa/yawa/DatabaseLoader.java
@@ -0,0 +1,25 @@
+package it.unisa.yawa;
+
+import it.unisa.yawa.model.Book;
+import it.unisa.yawa.model.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+	private final BookRepository repository;
+
+	@Autowired
+	public DatabaseLoader(BookRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Book("The Alchemist", "Paulo Coelho"));
+		this.repository.save(new Book("Fahrenheit 451", "Ray Bradbury"));
+	}
+
+}
