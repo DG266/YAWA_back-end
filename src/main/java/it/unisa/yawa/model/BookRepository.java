e2e75225604c2904dbbf4d03139facaf1945bdf4
dg266 <daniele.galloppo@protonmail.com>
2023-12-27 19:17:36 +0100

    Add JpaRepository for the Book entity

---
+++ src/main/java/it/unisa/yawa/model/BookRepository.java
@@ -0,0 +1,9 @@
+package it.unisa.yawa.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+	List<Book> findByTitleContaining(String title);
+}
