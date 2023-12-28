57561bf00149c9d7f45a1d74387a3c65965db484
dg266 <daniele.galloppo@protonmail.com>
2023-12-28 02:39:38 +0100

    Update BookRepository configuration

--- src/main/java/it/unisa/yawa/model/BookRepository.java
+++ src/main/java/it/unisa/yawa/model/BookRepository.java
@@ -1,9 +1,13 @@
 package it.unisa.yawa.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
+@RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Long> {
 	List<Book> findByTitleContaining(String title);
 }
---
+++ src/main/java/it/unisa/yawa/model/BookRepositoryRestConfigurer.java
@@ -0,0 +1,15 @@
+package it.unisa.yawa.model;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class BookRepositoryRestConfigurer implements RepositoryRestConfigurer {
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Book.class);
+	}
+}
